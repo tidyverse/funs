97798d75e76281d2ec5043462bc14a66797d99c9
DavisVaughan <davis@rstudio.com>
2020-10-08 08:43:18 -0400

    Rewrite `if_else()` so that `size` comes from `condition`
    Also updates style to use `_common()` functions and avoid two calls to `is.na()`

--- tests/testthat/test-if_else.R
+++ tests/testthat/test-if_else.R
@@ -3,11 +3,8 @@ test_that("result has expected values", {
   expect_equal(if_else(c(TRUE, FALSE, NA), 1, 2, 3), c(1, 2, 3))
 })
 
-test_that("size comes from all inputs", {
+test_that("size comes from condition", {
   expect_vector(if_else(rep(TRUE, 3), 1, 1), size = 3)
-  expect_vector(if_else(TRUE, 1:3, 1), size = 3)
-  expect_vector(if_else(TRUE, 1, 1:3), size = 3)
-  expect_vector(if_else(TRUE, 1, 1, 1:3), size = 3)
 })
 
 test_that("ptype come from true/false/na", {
@@ -18,7 +15,11 @@ test_that("ptype come from true/false/na", {
 
 test_that("if_else() errors informatively", {
   expect_snapshot(error = TRUE, {
-    if_else(1:2, 1:3, 1)
+    if_else(TRUE, 1:3, 1)
+    if_else(TRUE, 1, 1:3)
+    if_else(TRUE, 1, 1, 1:3)
+    if_else(c(TRUE, FALSE), 1:3, 1)
+
     if_else(1, 1, "2")
   })
 })
