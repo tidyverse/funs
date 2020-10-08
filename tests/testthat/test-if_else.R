d83dfb340e1941fc37ba441bf70adc2b44d1968c
DavisVaughan <davis@rstudio.com>
2020-10-08 08:43:18 -0400

    Rewrite `if_else()` so that `size` comes from `condition`
    Also updates style to use `_common()` functions and avoid two calls to `is.na()`

--- tests/testthat/test-if_else.R
+++ tests/testthat/test-if_else.R
@@ -3,13 +3,13 @@ test_that("result has expected values", {
   expect_equal(if_else(c(TRUE, FALSE, NA), 1, 2, 3), c(1, 2, 3))
 })
 
-test_that("size comes from all inputs", {
+test_that("size comes from condition", {
   expect_vector(if_else(rep(TRUE, 3), 1, 1), size = 3)
-  expect_vector(if_else(TRUE, 1:3, 1), size = 3)
-  expect_vector(if_else(TRUE, 1, 1:3), size = 3)
-  expect_vector(if_else(TRUE, 1, 1, 1:3), size = 3)
 
-  expect_error(if_else(1:2, 1:3, 1), class = "vctrs_error_incompatible_size")
+  expect_error(if_else(TRUE, 1:3, 1), class = "vctrs_error_incompatible_size")
+  expect_error(if_else(TRUE, 1, 1:3), class = "vctrs_error_incompatible_size")
+  expect_error(if_else(TRUE, 1, 1, 1:3), class = "vctrs_error_incompatible_size")
+  expect_error(if_else(c(TRUE, FALSE), 1:3, 1), class = "vctrs_error_incompatible_size")
 })
 
 test_that("ptype come from true/false/na", {
@@ -17,5 +17,5 @@ test_that("ptype come from true/false/na", {
   expect_vector(if_else(TRUE, 1.5, 1L), ptype = double())
   expect_vector(if_else(TRUE, 1L, 1L, 1.5), ptype = double())
 
-  expect_error(if_else(1, 1, "2"), class = "vctrs_error_incompatible_type")
+  expect_error(if_else(TRUE, 1, "2"), class = "vctrs_error_incompatible_type")
 })
