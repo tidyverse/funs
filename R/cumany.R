1cf095156caaa195484e5c07cecbaf17602ac7a2
Romain Francois <romain@rstudio.com>
2021-04-30 17:35:46 +0200

    doc skeleton

--- R/cumany.R
+++ R/cumany.R
@@ -1,11 +1,18 @@
+#' Cumulative versions of any() and all()
+#'
+#' @param x A logical vector
+#'
+#' @return A logical vector the same size as `x`
+#'
 #' @export
 cumany <- function(x) {
   vec_assert(x, logical())
   .Call(funs_cumany, x)
 }
 
+#' @rdname cumany
 #' @export
-cumaall <- function(x) {
+cumall <- function(x) {
   vec_assert(x, logical())
   .Call(funs_cumall, x)
 }
