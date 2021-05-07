2df35f907c5b4b4c8949a19b115fd7dfc3c6a3a0
Romain Francois <romain@rstudio.com>
2021-05-07 10:17:38 +0200

    play with the idea of patch() + when()

--- R/recode_when.R
+++ R/recode_when.R
@@ -40,3 +40,46 @@ recode_when <- function(x, ...) {
   }
   x
 }
+
+#' @export
+when <- function(what, with) {
+  patch_env <- context_peek("patch_env", "when()", "`patch()`")
+  x <- patch_env$x
+  step <- patch_env$step
+  arg <- glue("..{step}")
+
+  if (is_formula(what)) {
+    what <- as_function(what)
+  }
+  if (is_function(what)) {
+    selected <- vec_assert(what(x), ptype = logical(), size = vec_size(x), arg = arg)
+  } else if(identical(what, default_sentinel)) {
+    selected <- !patch_env$touched
+  } else {
+    selected <- vec_in(x, what, needles_arg = arg, haystack_arg = "x")
+  }
+
+  list(selected = selected, replacement = with)
+}
+
+#' @export
+patch <- function(x, ...) {
+  step <- 0L
+  default <- default_sentinel
+  env <- environment()
+  context_local("patch_env", env)
+  dots <- enexprs(...)
+
+  n <- vec_size(x)
+  touched <- logical(length = n)
+  for (i in seq_along(dots)) {
+    step <- i
+    results <- eval_bare(dots[[i]], env = env)
+
+    vec_slice(x, results$selected) <- results$replacement
+
+    touched <- touched | results$selected
+  }
+
+  x
+}
