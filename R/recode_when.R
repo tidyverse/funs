549f11ab43513e7b91ece6bde0593f7fbbe2b4fb
Romain Francois <romain@rstudio.com>
2021-05-19 15:52:07 +0200

    attempt at case(when())

--- R/recode_when.R
+++ R/recode_when.R
@@ -43,7 +43,7 @@ recode_when <- function(x, ...) {
 
 #' @export
 when <- function(what, ...) {
-  patch_env <- context_peek("patch_env", "when()", "`patch()`")
+  patch_env <- context_peek("patch_env", "when()", "`patch()` or `case()`")
   x <- patch_env$x
   step <- patch_env$step
   arg <- glue("..{step}")
@@ -52,12 +52,14 @@ when <- function(what, ...) {
     what <- as_function(what)
   }
   if (is_function(what)) {
+    if (inherits(x, "case_sentinel")) abort("cannot use function in case()")
     selected <- vec_assert(what(x), ptype = logical(), size = vec_size(x), arg = arg)
   } else if(identical(what, default_sentinel)) {
     selected <- !patch_env$touched
   } else if (is.logical(what)) {
     selected <- what
   } else {
+    if (inherits(x, "case_sentinel")) abort("cannot use values in case()")
     selected <- vec_in(x, what, needles_arg = arg, haystack_arg = "x")
   }
 
@@ -87,6 +89,7 @@ patch <- function(x, ...) {
     results <- eval_bare(dots[[i]], env = env(caller, default = default_sentinel))
 
     size_replacement <- vec_size(results$replacement)
+    # TODO: deal with selected here as in case(when())
     if (size_replacement == 1L) {
       vec_slice(x, results$selected) <- results$replacement
     } else if (size_replacement == n){
@@ -105,3 +108,55 @@ patch <- function(x, ...) {
 
   x
 }
+
+#' @export
+case <- function(...) {
+  step <- 0L
+  caller <- caller_env()
+  env <- environment()
+  context_local("patch_env", env)
+  patch_type <- "patch"
+  dots <- enexprs(...)
+
+  x <- structure(list(), class = "case_sentinel")
+  touched <- NULL
+  out <- NULL
+
+  for (i in seq_along(dots)) {
+    step <- i
+    results <- eval_bare(dots[[i]], env = env(caller, default = default_sentinel))
+
+    n <- vec_size(results$selected)
+    if (step == 1) {
+      out <- vec_init(results$replacement, n)
+    }
+
+    size_replacement <- vec_size(results$replacement)
+    selected <- results$selected
+    if (!is.null(touched)) {
+      selected <- selected & !touched
+    }
+    if (size_replacement == 1L) {
+      vec_slice(out, selected) <- results$replacement
+    } else if (size_replacement == n){
+      vec_slice(out, selected) <- vec_slice(results$replacement, selected)
+    } else {
+      # TODO: maybe check this is a when() call and extract the with=
+      abort(c(
+        glue("Incompatible replacement size in `..{ i }`"),
+        x = glue("`..{i}` should be size 1 or { n }, not { size_replacement }."),
+        i = glue("`..{i}` is `{as_label(dots[[i]])}`. ")
+      ))
+    }
+
+    touched <- if (is.null(touched)) {
+      results$selected
+    } else(
+      touched | results$selected
+    )
+  }
+
+  out
+}
+
+
