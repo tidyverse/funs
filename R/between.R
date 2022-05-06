e0c01d5fcafefb24ee1d40c0e7e6f2bef2b4b0d9
DavisVaughan <davis@rstudio.com>
2022-05-06 12:31:45 -0400

    Cast `left/right` to the type of `x` and recycle to the size of `x`

--- R/between.R
+++ R/between.R
@@ -1,13 +1,16 @@
 #' Find all values within a range
 #'
-#' This is a shortcut for `x >= left & x <= right`,
+#' This is essentially a shortcut for `x >= left & x <= right`, but it also
+#' retains the size of `x` and casts both `left` and `right` to the type of
+#' `x` before making the comparison.
 #'
-#' @param x A vector of values
-#' @param left,right Boundary values.
-#' @param bounds One of \verb{[]}, \verb{[)}, \verb{()}, or `()`, which defines whether the
-#'   boundary is inclusive (`[` / `]`) or exclusive (`(` / `)`).
-#' @return A logical vector. The length will be determined by the common
-#'   length of `x`, `left`, and `right`.
+#' @param x A vector
+#' @param left,right Boundary values. Both `left` and `right` are recycled to
+#'   the size of `x` and are cast to the type of `x`.
+#' @param bounds One of \verb{"[]"}, \verb{"[)"}, \verb{"(]"}, or \verb{"()"},
+#'   which defines whether the boundary is inclusive (with `[` or `]`) or
+#'   exclusive (with `(` or `)`).
+#' @return A logical vector the same size as `x`.
 #' @export
 #' @examples
 #' between(c(1:10, NA), 4, 6)
@@ -16,14 +19,40 @@
 #' today <- Sys.Date()
 #' between(today, today - 1, today + 1)
 between <- function(x, left, right, bounds = "[]") {
+  args <- list(left = left, right = right)
+  args <- vec_cast_common(!!!args, .to = x)
+  args <- vec_recycle_common(!!!args, .size = vec_size(x))
+  left <- args[[1L]]
+  right <- args[[2L]]
+
+  bounds <- check_bounds(bounds)
   bounds <- switch(bounds,
     "[]" = list(`>=`, `<=`),
     "[)" = list(`>=`, `<`),
     "(]" = list(`>`, `<=`),
     "()" = list(`>`, `<`),
-    abort("Unknown `bounds` specification.")
+    abort("Unknown `bounds` specification.", .internal = TRUE)
   )
 
-  bounds[[1]](vec_compare(x, left), 0) &
-    bounds[[2]](vec_compare(x, right), 0)
+  fn_left <- bounds[[1L]]
+  fn_right <- bounds[[2L]]
+
+  left <- vec_compare(x, left)
+  left <- fn_left(left, 0L)
+
+  right <- vec_compare(x, right)
+  right <- fn_right(right, 0L)
+
+  left & right
+}
+
+check_bounds <- function(bounds, ..., error_call = caller_env()) {
+  check_dots_empty0(...)
+
+  arg_match0(
+    arg = bounds,
+    values = c("[]", "[)", "(]", "()"),
+    arg_nm = "bounds",
+    error_call = error_call
+  )
 }
--- tests/testthat/_snaps/between.md
+++ tests/testthat/_snaps/between.md
@@ -1,8 +1,47 @@
-# between() errors
+# casts `left` and `right` to the type of `x` (#74)
+
+    Code
+      between(1L, 1.5, 2L)
+    Condition
+      Error in `between()`:
+      ! Can't convert from `left` <double> to <integer> due to loss of precision.
+      * Locations: 1
+
+---
+
+    Code
+      between(1L, 1L, 2.5)
+    Condition
+      Error in `between()`:
+      ! Can't convert from `right` <double> to <integer> due to loss of precision.
+      * Locations: 1
+
+# recycles `left` and `right` to the size of `x` (#74)
+
+    Code
+      between(1:3, 1:2, 1L)
+    Condition
+      Error in `between()`:
+      ! Can't recycle `left` (size 2) to size 3.
+
+---
+
+    Code
+      between(1:3, 1L, 1:2)
+    Condition
+      Error in `between()`:
+      ! Can't recycle `right` (size 2) to size 3.
+
+# validates `bounds`
 
     Code
       between(1:2, 1, 1, bounds = "")
     Condition
       Error in `between()`:
-      ! Unknown `bounds` specification.
+      ! `bounds` must be one of "[]", "[)", "(]", or "()", not "".
+    Code
+      between(1:2, 1, 1, bounds = 1)
+    Condition
+      Error in `arg_match0()`:
+      ! `bounds` must be a string or character vector.
 
