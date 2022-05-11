77c050522d884f957df099dd0de38268a400fc12
DavisVaughan <davis@rstudio.com>
2022-05-11 15:18:16 -0400

    Refine documentation using review feedback

--- R/coalesce.R
+++ R/coalesce.R
@@ -6,33 +6,19 @@
 #'
 #' @param ... One or more vectors.
 #' @param .ptype The type to cast the vectors in `...` to. If `NULL`, the
-#'   vectors will be cast to a common type, which is consistent with SQL.
+#'   vectors will be cast to their common type, which is consistent with SQL.
 #' @param .size The size to recycle the vectors in `...` to. If `NULL`, the
-#'   vectors will be recycled to a common size.
+#'   vectors will be recycled to their common size.
 #' @export
 #' @examples
 #' # Use a single value to replace all missing values
 #' x <- sample(c(1:5, NA, NA, NA))
 #' coalesce(x, 0L)
 #'
-#' # In cases like the one above, you may want to require that the return
-#' # type has the same type as the first input (here, `x`) rather than the
-#' # common type of all inputs. To do that, set the `.ptype` argument to a
-#' # vector with your preferred type.
-#' typeof(coalesce(x, 0))
-#' typeof(coalesce(x, 0, .ptype = x))
-#'
 #' # The equivalent to a missing value in a list is NULL
 #' coalesce(list(1, 2, NULL), list(NA))
 #'
-#' # data frames are coalesced, row by row, and the output will contain
-#' # columns that appear in any input
-#' coalesce(
-#'   data.frame(x = c(1, NA)),
-#'   data.frame(x = c(NA, 2), y = 3)
-#' )
-#'
-#' # Or match together a complete vector from missing pieces
+#' # Or generate a complete vector from partially missing pieces
 #' y <- c(1, 2, NA, NA, 5)
 #' z <- c(NA, NA, 3, 4, 5)
 #' coalesce(y, z)
